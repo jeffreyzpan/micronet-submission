ab51be3f5baa3046e1d72bd6744b9e66294306f7
Kuan Wang <304600203@qq.com>
2019-10-11 11:15:49 +0800

    Update run_manager.py
    index of nn.conv2d

--- models/networks/run_manager.py
+++ models/networks/run_manager.py
@@ -433,7 +433,8 @@ class RunManager:
 		quantize_index = []
 		for idx, module in enumerate(self.net.modules()):
 			print(type(module))
-			if type(module) == layers.DepthConvLayer:
+			if type(module) == in [nn.Conv2d, nn.Linear]:
 				quantize_index.append(idx)
 			print(idx, '->', module)
 		print(quantize_index)
+		
